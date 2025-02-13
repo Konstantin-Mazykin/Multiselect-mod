d42727cc253be7be83c4610aee556c957589bcf4
Konstantin-Mazykin <konstantin.mazykin@gmail.com>
2025-02-13 11:39:26 +0200

    Fix errors

--- js/view.js
+++ js/view.js
@@ -1,19 +1,20 @@
-function init(array, htmlElement) {
+function render(array, htmlElement) {
     for (let i = 0; i < array.length; i++) {
-        let section = document.createElement("li");
-        section.classList.add("multiselect__item");
+        let element = document.createElement("li");
+        element.classList.add("multiselect__item");
 
-        section.setAttribute("tabindex", '0');
-        section.setAttribute("aria-checked", 'false');
-        section.setAttribute("data-value", `${array[i].toLowerCase()}`);
+        element.setAttribute("tabindex", '0');
+        element.setAttribute("aria-checked", 'false');
+        element.setAttribute("data-value", `${array[i].toLowerCase()}`);
 
-        section.insertAdjacentHTML("beforeend", `<input class="multiselect__checkbox-input" type="checkbox" id="item- ${i + 1}" />`);
-        section.insertAdjacentHTML("beforeend", `<label class="multiselect__checkbox-label" for="item-${i + 1}">${array[i]}</label>`);
+        element.insertAdjacentHTML("beforeend", `<input class="multiselect__checkbox-input" type="checkbox" id="item-${i + 1}">`);
+        element.insertAdjacentHTML("beforeend", `<label class="multiselect__checkbox-label" for="item-${i + 1}">${array[i]}</label>`);
 
-        htmlElement.append(section);
+        htmlElement.append(element);
     }
 }
 
 const itemsList = ['Kyiv', 'Kharkiv', 'Lviv', 'Odessa', 'Dnipro'];
 
-init(itemsList, document.querySelector(".multiselect__list"));
+render(itemsList, document.querySelector(".multiselect__list"));
+
